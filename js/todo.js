8c47595a72e29013f9b2ebfc0914444032f5e6dd
Minn93 <tlsghktks8@gmail.com>
2021-10-16 21:14:43 +0900

    doing it

---
+++ js/todo.js
@@ -0,0 +1,28 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(){
+    console.log("click");
+}
+
+function paintToDo(newToDo){
+    const li= document.createElement("li");
+    const span= document.createElement("span");
+    span.innerText= newToDo;
+    const button= document.createElement("button");
+    button.innerText= "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);   
+    li.appendChild(button); 
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value ="";
+    paintToDo(newToDo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
