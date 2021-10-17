a9b2006b2b7ec80e63e7ba276c73cf24982d08c0
Minn93 <tlsghktks8@gmail.com>
2021-10-17 23:55:13 +0900

    doing it

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-function deleteToDo(){
-    console.log("click");
+const TODOS_KEY= "todos";
+
+const toDos= [];
+
+function saveToDos(){
+    localStorage .setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+function deleteToDo(event){
+    const li= event.target.parentElement;
+    li.remove();
+}
 function paintToDo(newToDo){
     const li= document.createElement("li");
     const span= document.createElement("span");
@@ -22,7 +30,17 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value ="";
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos= localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos= parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
